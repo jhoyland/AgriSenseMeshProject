e7b8a0465a05d5b7a57e31cb56c10fddb7cd5f34
jhoyland <hoyland.jd@gmail.com>
2018-05-30 18:15:17 +0000

    Starting ATTiny SPI slave experiment

---
+++ experiments/attiny-spi-slave/spi-slave-test1.c
@@ -0,0 +1,14 @@
+
+#define F_CPU 1000000L
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+/* 
+References:
+
+see: weathergadget.wordpress.com/2016/05/19/usi-spi-slave-communication/
+
+
+*/
+
