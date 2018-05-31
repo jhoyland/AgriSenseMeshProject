6d790388a4724bbe7d72229c4ae95f24425b8f54
jhoyland <hoyland.jd@gmail.com>
2018-05-31 05:33:09 +0000

    Finished overflow interrupt vector

--- experiments/attiny-spi-slave/spi-slave-test1.c
+++ experiments/attiny-spi-slave/spi-slave-test1.c
@@ -17,6 +17,8 @@ see: weathergadget.wordpress.com/2016/05/19/usi-spi-slave-communication/
 #define USCK	PB2
 #define D1	PB0
 
+volatile char cmdcode = 0;
+
 void setup_spi(void)
 {
 	cli(); /*Disable interrupts*/
@@ -30,6 +32,8 @@ void setup_spi(void)
 	PCMSK |= 1<<CS; /* Interrupt will be set on pin changes of CS */
 	GIMSK |= 1<<PCIE; /* Pin change interrupts will be enabled */
 
+	delay(500);
+
 	sei(); /* Enable interrupts */
 }
 
@@ -37,11 +41,49 @@ ISR(PCINT0_vect)
 {
 	if( PINB & (1<<CS) ) /*If true then CS pin has just gone high - SPI transaction finished */
 	{
-
+		USICR &= ~(1<<USIOIE);
 	}
 	else   /* Start of an SPI transaction */
 	{
-		
+		cmdcode = 0;
+		USICR |= (1<<USIOIE);
+		USISR = 1<<USIOIF;   /* resets the overflow flag - weirdly  */
 	}
+
+}
+
+ISR(USI_OVF_vect)
+{
+	switch(cmdcode){
+		case 0:
+			cmdcode = USIDR;
+			USISR = 1<<USIOIF;
+			break;
+		case 'A':
+
+			USIDR = 5;
+			USISR = 1<<USIOIF;
+			break;
+
+		case 'B':
+
+			USIDR = 9;
+			USISR = 1<<USIOIF;
+			break;
+
+		default:
+
+			USIDR = cmdcode;
+			USISR = 1<<USIOIF; 
+
+	}
+}
+
+
+int main()
+{	
+	setup_spi();
+	for(;;);
+	return 0;
 
 }
